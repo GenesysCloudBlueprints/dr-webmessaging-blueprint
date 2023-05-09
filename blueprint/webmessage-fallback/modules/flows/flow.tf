b12f26a382eb20134c54e630a7793019137e3466
John Carnell <john.carnell@genesys.com>
2023-05-09 14:08:59 -0400

    Working on the fallback deploy

--- blueprint/webmessage-fallback/modules/flows/flow.tf
+++ blueprint/webmessage-fallback/modules/flows/flow.tf
@@ -1,6 +1,5 @@
 resource "genesyscloud_flow" "deploy_archy_flow_chat" {
-  depends_on=[genesyscloud_flow.deploy_archy_flow_emergency_bot,
-              genesyscloud_flow.deploy_archy_flow_bot]
+  depends_on=[genesyscloud_flow.deploy_archy_flow_emergency_bot]
   filepath = "${path.module}/MyWebMessenger.yaml"
   file_content_hash = filesha256("${path.module}/MyWebMessenger.yaml") 
   substitutions = {
